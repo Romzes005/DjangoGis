03fee9d9a856dcc02ac261e389197f5f5b29092f
Vsevolod Demidov <fondad@ukr.net>
2018-04-05 13:50:27 +0300

    Articles, About templates were added. Static was added.

--- gis/views.py
+++ gis/views.py
@@ -1,8 +1,28 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from gis.models import Articles
+
+
 def home(request):
     # return HttpResponse("Hello world")
-    return render(request, 'gis/home.html')
+    # return render(request, 'gis/home.html')
+    articles = Articles.objects.all()
+    context = {
+        'articles': articles
+    }
+    return render(request, 'gis/home.html', context)
+
+
+def show_articles(request, article_id):
+    article = get_object_or_404(Articles, id=article_id)
+    return render(request, 'gis/article.html', {'article': article})
+
+
+def about(request):
+    return render(request, 'gis/about.html')
+
+
+def mymap(request):
+    return render(request, 'gis/map.html')
